c8a40ea4cf2d1539765cdbd01c222f7aa5f85051
dai <852877357@qq.com>
2016-07-09 15:50:53 +0800

    入口完成

--- entry.c
+++ entry.c
@@ -1,74 +1,100 @@
-//entry.c
 #include "minicrt.h"
 
 #ifdef WIN32
-#include<Windows.h>
+
+#include <Windows.h>
+
 #endif
 
-extern int main(int argc,char *argv[]);
+
+extern int main(int arge, char *argv[]);
 void exit(int);
 
-static void crt_fatal_error(const char* msg){
-	exit(1);
+
+static void crt_fatal_error(const char *msg)
+{
+    printf("fatal error: %s", msg);
+    exit(1);
 }
 
-void mini_crt_entry(void){
-	int ret;
-//è§£æå‚æ•°
-#ifdef WIN32				//windows
 
-	int flag = 0;
-	int argc = 0;
-	char *argv[16];			//æœ€å¤š16ä¸ªå‚æ•°
-	char* c1 = GetCommandLineA();	//è·å–è¿›ç¨‹çš„å‘½ä»¤è¡Œå‚æ•°
+void mini_crt_entry(void)
+{
+    int ret = 0;
 
-	//è§£é‡Šå‘½ä»¤è¡Œ
-	argv[0] = c1;
-	argc++;
-	while(*c1){
-		if(*c1 == '\"')
-			if(flag == 0)flag =1;
-			else flag = 0;
-		else if(*c1 == '' && flag==0){
-			if(*(c1+1)){
-				argv[argc] = c1+1;
-				argc++;
-			}
-			*c1='\0';
-		}
-		c1++;
-	}	
-#else					//linux
-	int argc;
-	char** argv;
-
-	char* ebp_reg=0;
-	//ebp_reg = %ebp;
-	asm("movl %%ebp,%0 \n":"=r"(ebp_reg));
-
-	argc = *(int*)(ebp_reg + 4);
-	argv = (char**)(ebp_reg+8);
-#endif
-//åˆå§‹åŒ–å †ï¼Œio
-	if(!mini_crt_heap_init())
-		crt_fatal_error("heap initialize failed");
-
-	if(!mini_crt_io_init())
-		crt_fatal_error("IO initialize failed");
-//è°ƒç”¨å‡½æ•°main
-	ret = main(argc,argv);
-	exit(ret);
-}	
-
-void exit(int exitcode){
-	//mini_crt_call_exit_routine();
 #ifdef WIN32
-	ExitProcess(exitcode);
+    int flag = 0;
+    int argc = 0;
+    char *argv[16];  // ×î¶àÖ§³Ö16¸ö²ÎÊı
+    char *cl = GetCommandLine(); // WindowsÌá¹©¸ÃAPI»ñÈ¡½ø³ÌµÄÃüÁîĞĞ²ÎÊı
+    
+    // ½âÎöÃüÁîĞĞ
+    argv[0] = cl;
+    argc++;
+    while (*cl)
+    {
+        if (*cl == '\"' && flag == 0)
+        {
+            flag = 1;
+        }
+        else if (*cl == ' ' && flag == 0)
+        {
+            if (*(cl + 1))
+            {
+                argv[argc] = cl + 1;
+                argc++;
+            }
+            *cl = '\0';
+        }
+
+        cl++;
+    }
+    
 #else
-	asm("movl %0,%%ebx \n\t"
-			"movl $1,%%eax \n\t"
-			"int $0x80   \n\t"
-			"hlt \n\t"::"m"(exitcode)
-			);
+    int argc = 0;
+    char **argv;
+
+    char *ebp_reg = NULL;
+    asm("movq %%rbp, %0  \n" : "=r"(ebp_reg));
+    
+    argc = *(int *)(ebp_reg + 8);
+    argv = (char **)(ebp_reg + 16);
 #endif
+
+    if (!mini_crt_heap_init())
+    {
+        crt_fatal_error("Heap Initialize Failed!\n");
+    }
+
+    if (!mini_crt_io_init())
+    {
+        crt_fatal_error("IO Initialize Failed!\n");
+    }
+    
+    // ³õÊ¼»¯²¿·Ö
+    ret = main(argc, argv);
+
+    // ½áÊø²¿·Ö
+    exit(ret);
 }
+
+
+void exit(int exitCode)
+{
+    // mini_crt_call_exit_routine();
+
+#ifdef WIN32
+    ExitProcess(exitCode);
+#else
+    asm( "movl %0, %%ebx \n\t"
+         "movl $1, %%eax \n\t"
+         "int $0x80      \n\t"
+         "hlt            \n\t"::"m"(exitCode));
+
+#endif
+
+}
+
+
+
+
